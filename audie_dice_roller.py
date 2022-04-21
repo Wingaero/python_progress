175496e3dc8fa8eb4e72d9edd39820e8825f9f06
Wingaero <104048302+Wingaero@users.noreply.github.com>
2022-04-20 19:53:11 -0700

    python_day_2_upd

---
+++ audie_dice_roller.py
@@ -0,0 +1,11 @@
+import random
+
+is_active = True
+while is_active:
+    num_of_rolls = int(input('How many dice are you rolling?: '))
+    num_of_sides = int(input('How many sides are on your dice?: '))
+    dice_rolls = [random.choice(range(1, num_of_sides+1)) for rolls in range(num_of_rolls)]
+    print(dice_rolls, 'You rolled', sum(rolls for rolls in dice_rolls))
+
+    is_restart = input('Would you like to roll more dice? (y/n): ')
+    if is_restart == 'n': is_active = False
--- python_day_2.py
+++ python_day_2.py
@@ -1,30 +1,22 @@
 from random import randint
+is_active = True
 
-number_of_loops = 0
+while is_active:
 
-start = input("Would you like to start? (y/n)")
+    number_of_rolls = input("How many dice are you rolling?")
+    size_of_dice = input("What size are the dice being used?")
+    print("Using " + number_of_rolls + " " + size_of_dice + "-sided dice.")
 
-while start == "y":
+    number_of_rolls = int(number_of_rolls)
+    size_of_dice = int(size_of_dice)
+    i = 1
+    rolls_list = []
 
-  number_of_rolls = input("How many dice are you rolling?")
-  size_of_dice = input("What size are the dice being used?")
-  print( "Using " + number_of_rolls + " " + size_of_dice + "-sided dice.")
-  number_of_loops += 1
+    while i <= number_of_rolls:
+        i += 1
+        rolls_list.append(randint(1, size_of_dice))
+        print(sum(rolls_list))
 
-  number_of_rolls = int(number_of_rolls)
-  size_of_dice = int(size_of_dice)
-  i = 1
-  rolls_list = []
-
-  while i <= number_of_rolls:
-    i += 1
-    rolls_list.append(randint(1,size_of_dice))
-    print(sum(rolls_list))
-
-  cont = input("Would you like to continue? (y/n)")
-
-  if cont == "n":
-    start = "n"
-  else: start = "y"
-
-else: print("Rolling halted.")
+    is_restart = input('Would you like to roll more dice? (y/n): ')
+    if is_restart == 'n':
+        is_active = False
